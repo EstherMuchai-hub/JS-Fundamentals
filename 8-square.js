6e5452ff75e477b83b9d18b41f9520ff193d836c
Esther muchai <muchaiesther23@gmail.com>
2025-09-21 20:42:54 +0300

    Update 8-square.js
    alx task 8 square js

--- 8-square.js
+++ 8-square.js
@@ -1,1 +1,14 @@
+// 9-square.js
+
+const size = Number(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log("Missing size");
+} else {
+  let i = 0;
+  while (i < size) {
+    console.log("X".repeat(size));
+    i++;
+  }
+}
 
