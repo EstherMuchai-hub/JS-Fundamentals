8377123b41f22b5b76876de8cd4d447757352e90
Esther muchai <muchaiesther23@gmail.com>
2025-09-21 20:34:09 +0300

    Update 5-to_integer.js
    ALX TASK 5 INTEGER

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,1 +1,11 @@
+// 5-to_integer.js
+
+const arg = process.argv[2];
+const number = Number(arg);
+
+if (!isNaN(number)) {
+  console.log(`My number: ${number}`);
+} else {
+  console.log("Not a number");
+}
 
